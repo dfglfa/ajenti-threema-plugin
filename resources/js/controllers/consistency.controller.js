54a2591df7d0d6b79bc53fbe4a57c9835b7183bb
jcharra <johannes.charra@gmail.com>
2023-05-21 15:57:10 +0200

    add consistency check button to index page

--- resources/js/controllers/consistency.controller.js
+++ resources/js/controllers/consistency.controller.js
@@ -1,35 +1,30 @@
-angular
-  .module("example.threema_connector")
-  .controller(
-    "ThreemaConnectorConcistencyController",
-    function ($scope, $http, pageTitle, gettext, notify) {
-      pageTitle.set(gettext("Consistency"));
+angular.module("example.threema_connector").controller("ThreemaConnectorConcistencyController", function ($scope, $http, pageTitle, gettext, notify) {
+  pageTitle.set(gettext("Consistency"));
 
-      $scope.check = () => {
-        return $http
-          .get("/api/threema_connector/credentials/check")
-          .then((resp) => {
-            $scope.results = resp.data;
-          });
-      };
+  $scope.updatePending = false;
 
-      $scope.updatePending = false;
+  check();
 
-      $scope.applySuggestion = (threemaId, changedName) => {
-        $scope.updatePending = true;
-        $http
-          .post("/api/threema_connector/credentials/update", {
-            threemaId,
-            changedName,
-          })
-          .then((resp) => {
-            $scope.check().then(() => {
-              $scope.updatePending = false;
-            });
-          })
-          .catch(() => {
-            $scope.updatePending = false;
-          });
-      };
-    }
-  );
+  $scope.applySuggestion = (threemaId, changedName) => {
+    $scope.updatePending = true;
+    $http
+      .post("/api/threema_connector/credentials/update", {
+        threemaId,
+        changedName,
+      })
+      .then((resp) => {
+        $scope.check().then(() => {
+          $scope.updatePending = false;
+        });
+      })
+      .catch(() => {
+        $scope.updatePending = false;
+      });
+  };
+
+  function check() {
+    return $http.post("/api/threema_connector/credentials/check", {}).then((resp) => {
+      $scope.results = resp.data;
+    });
+  }
+});
