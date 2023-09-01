83e29db460d853eec432fa71264a56bee5256e4b
jcharra <johannes.charra@gmail.com>
2023-09-01 08:12:08 +0200

    add teachers tab

--- resources/js/controllers/index.controller.js
+++ resources/js/controllers/index.controller.js
@@ -1,3 +1,34 @@
-angular.module("dfglfa.threema_connector").controller("ThreemaConnectorIndexController", function ($scope) {
-  $scope.activetab = 0;
-});
+angular
+  .module("dfglfa.threema_connector")
+  .constant("TEACHER_PREFIX", "L_")
+  .controller("ThreemaConnectorIndexController", function ($scope, $http, TEACHER_PREFIX) {
+    $scope.activetab = 0;
+
+    loadCredentials();
+
+    function loadCredentials() {
+      $http.get("/api/threema_connector/credentials").then((resp) => {
+        sortByUserType(resp.data);
+      });
+    }
+
+    function sortByUserType(credsList) {
+      const studentsCreds = [];
+      const teachersCreds = [];
+
+      for (let c of credsList) {
+        if (c.username && c.username.startsWith(TEACHER_PREFIX)) {
+          teachersCreds.push(c);
+        } else {
+          studentsCreds.push(c);
+        }
+      }
+
+      $scope.studentsCredentials = studentsCreds;
+      $scope.teachersCredentials = teachersCreds;
+    }
+
+    $scope.onCredsChange = () => {
+      sortByUserType($scope.studentsCredentials.concat($scope.teachersCredentials));
+    };
+  });
--- resources/js/directives/userList.js
+++ resources/js/directives/userList.js
@@ -3,5 +3,9 @@ angular.module("dfglfa.threema_connector").directive("userList", function () {
     restrict: "E",
     templateUrl: "/threema_connector:resources/partial/userList.html",
     controller: "ThreemaUserListController",
+    scope: {
+      credentials: "=",
+      onCredentialsChange: "=",
+    },
   };
 });
