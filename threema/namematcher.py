b743476cec036338a9170dee4e423b65bb6ee8e3
jcharra <johannes.charra@gmail.com>
2023-09-11 14:44:37 +0200

    change diff param

--- threema/namematcher.py
+++ threema/namematcher.py
@@ -56,7 +56,7 @@ class NameMatcher:
 
     def findMatches(self, name) -> list:
         match = difflib.get_close_matches(
-            name, self.nameToClass.keys(), 2, cutoff=0.6)
+            name, self.nameToClass.keys(), 2, cutoff=0.7)
         if match:
             return [(res, self.nameToClass[res]) for res in match]
         else:
