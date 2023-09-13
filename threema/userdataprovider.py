5537706f5c260103575e73f1857d3baa3b04b688
jcharra <johannes.charra@gmail.com>
2023-09-13 09:34:07 +0200

    make UserDataProvider failsafe

--- threema/userdataprovider.py
+++ threema/userdataprovider.py
@@ -1,9 +1,27 @@
-from aj.plugins.lmn_common.ldap.requests import LMNLdapRequests
+import csv
+import logging
+
+from threema.config_loader import getStudentsFileName
+
+try:
+    from aj.plugins.lmn_common.ldap.requests import LMNLdapRequests
+except ImportError:
+    LMNLdapRequests = None
 
 
 class UserDataProvider():
     def getUserData(self):
-        lr = LMNLdapRequests(None)
-        users_data = lr.get('/role/student', school_oriented=False)
-        users_data += lr.get('/role/teacher', school_oriented=False)
-        return users_data
+        user_data = []
+        if LMNLdapRequests:
+            logging.info("Fetching user data via LDAP")
+            lr = LMNLdapRequests(None)
+            user_data += lr.get('/role/student', school_oriented=False)
+            user_data += lr.get('/role/teacher', school_oriented=False)
+        else:
+            filename = getStudentsFileName()
+            logging.warn(
+                f"Fetching user data from example data file {filename}")
+            with open(filename, "r") as csv_file:
+                reader = csv.DictReader(csv_file, delimiter=",")
+                return [{"sn": rec['Nom'], "givenName": rec['Prenom'], "sophomorixAdminClass": rec["Classe"]} for rec in reader]
+        return user_data
