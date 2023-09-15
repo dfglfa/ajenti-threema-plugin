2a3e991aa98053a627129d2b032a5c69d3751d32
jcharra <johannes.charra@gmail.com>
2023-09-15 10:04:30 +0200

    use caching for fetching all users

--- threema/userclient.py
+++ threema/userclient.py
@@ -1,9 +1,12 @@
 import json
 import logging
+import time
 import requests
 import urllib
 
 from .datamodel import User
+
+USER_CACHE = {"timestamp": None, "users": []}
 
 
 class UserClient:
@@ -27,21 +30,27 @@ class UserClient:
             return []
 
     def getUsersWithLinkedCredentials(self):
-        url = f"{self.baseUrl}/users"
+        now = int(time.time())
+        if not USER_CACHE["timestamp"] or now - USER_CACHE["timestamp"] > 60:
+            logging.info("Fetching all users from threema")
+            url = f"{self.baseUrl}/users"
 
-        # Not ideal performance-wise ... consider caching or fetching each user
-        # record separately (= n requests for a class with n members)
-        params = {"pageSize": 2000}
+            # Not ideal performance-wise ... consider caching or fetching each user
+            # record separately (= n requests for a class with n members)
+            params = {"pageSize": 2000}
 
-        resp = requests.get(
-            url, params=params, headers=self.authHeader)
+            resp = requests.get(
+                url, params=params, headers=self.authHeader)
 
-        data = json.loads(resp.content)
-        users = data["users"]
+            data = json.loads(resp.content)
+            users = data["users"]
 
-        logging.info(f"**** Found {len(users)} users with credentials")
+            logging.info(f"**** Found {len(users)} users with credentials")
+            users_with_creds = {self._extractCredsForUser(u): u for u in users}
+            USER_CACHE["users"] = users_with_creds
+            USER_CACHE["timestamp"] = now
 
-        return {self._extractCredsForUser(u): u for u in users}
+        return USER_CACHE["users"]
 
     def _extractCredsForUser(self, user):
         for link in user["_links"]:
