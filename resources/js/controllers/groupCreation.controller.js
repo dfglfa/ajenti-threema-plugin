32ae107f4ff4577f391a2a8313f50b2df6558bb4
jcharra <johannes.charra@gmail.com>
2023-09-29 15:13:24 +0200

    do not show in result or add members that are already listed

--- resources/js/controllers/groupCreation.controller.js
+++ resources/js/controllers/groupCreation.controller.js
@@ -1,7 +1,8 @@
 angular.module("dfglfa.threema_connector").controller("ThreemaGroupCreationController", function ($scope, $http, $timeout, $location) {
   $scope.groupId = $location.search().id;
   $scope.groupName = undefined;
-  $scope.members = undefined;
+  $scope.existingMembers = undefined;
+  $scope.newMembers = [];
   $scope.searching = false;
   $scope.maxResults = 30;
 
@@ -24,7 +25,7 @@ angular.module("dfglfa.threema_connector").controller("ThreemaGroupCreationContr
     loadGroupInfo();
   } else {
     $scope.groupName = "";
-    $scope.members = [];
+    $scope.existingMembers = [];
   }
 
   function runSearch() {
@@ -40,8 +41,9 @@ angular.module("dfglfa.threema_connector").controller("ThreemaGroupCreationContr
       .get("/api/threema_connector/contacts", {})
       .then(({ data: contacts }) => {
         const results = [];
+        const alreadyListed = $scope.existingMembers.concat($scope.newMembers).map((m) => m.id);
         for (const c of contacts) {
-          if (!c.enabled) {
+          if (!c.enabled || alreadyListed.indexOf(c.id) !== -1) {
             continue;
           }
 
@@ -60,19 +62,20 @@ angular.module("dfglfa.threema_connector").controller("ThreemaGroupCreationContr
 
   function addResults() {
     const members = [];
-    const alreadyAdded = $scope.members.map((m) => m.id);
+    const alreadyAdded = $scope.newMembers.map((m) => m.id);
+    const existingIds = $scope.existingMembers.map((m) => m.id);
     for (const r of $scope.results) {
-      if (alreadyAdded.indexOf(r.id) === -1) {
+      if (alreadyAdded.indexOf(r.id) === -1 && existingIds.indexOf(r.id) === -1) {
         members.push(r);
       }
     }
-    $scope.members = $scope.members.concat(members);
+    $scope.newMembers = $scope.newMembers.concat(members);
     $scope.search = "";
     $scope.results = [];
   }
 
   function createGroup() {
-    return $http.post("/api/threema_connector/groups", { name: $scope.groupName, members: $scope.members.map((m) => m.id) }).then(() => {
+    return $http.post("/api/threema_connector/groups", { name: $scope.groupName, members: $scope.newMembers.map((m) => m.id) }).then(() => {
       $location.path("/view/threema_connector");
       console.log("Group successfully created");
     });
@@ -84,7 +87,7 @@ angular.module("dfglfa.threema_connector").controller("ThreemaGroupCreationContr
       for (const m of members) {
         mems.push({ id: m.id, firstName: m.firstName, lastName: m.lastName });
       }
-      $scope.members = mems;
+      $scope.existingMembers = mems;
     });
   }
 
