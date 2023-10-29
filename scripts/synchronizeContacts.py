b47774358a6ca5684680cee1ea57618396cc7013
jcharra <johannes.charra@gmail.com>
2023-10-29 14:19:13 +0100

    complete script to sync contacts

--- scripts/synchronizeContacts.py
+++ scripts/synchronizeContacts.py
@@ -10,4 +10,23 @@ from threema.threemaapi import ThreemaAdminClient
 client = ThreemaAdminClient()
 changes = client.findNormalizations()
 
-print("Found changes: ", changes)
+updates = changes.get("updates", [])
+print(f"\n*** Found {len(updates)} updates\n")
+for update in updates:
+    threemaId = update.get("threemaId")
+    firstname = update.get("firstNameNormalized")
+    lastname = update.get("lastNameNormalized")
+    enabled = update.get("enabled", True)
+    print(
+        f"Updating threemaId {threemaId} to {firstname} {lastname}, enabled: {enabled}")
+    client.applyContactChange(threemaId, firstname, lastname, enabled)
+
+missing = changes.get("missing", [])
+print(f"\n*** Found {len(missing)} missing contacts\n")
+for m in missing:
+    threemaId = m.get("threemaId")
+    firstName = m.get("firstName")
+    lastName = m.get("lastName")
+    print(
+        f"Creating contact for threemaId {threemaId} with firstname {firstName} and lastname {lastName}")
+    client.createContact(threemaId, firstName, lastName)
