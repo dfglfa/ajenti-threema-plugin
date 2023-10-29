793f4a59b70f97ce50c6fc82a0418c321fd0dcd0
jcharra <johannes.charra@gmail.com>
2023-10-29 15:30:49 +0100

    improve script format

--- scripts/synchronizeContacts.py
+++ scripts/synchronizeContacts.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 
 current_file_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_file_dir)
@@ -10,23 +11,31 @@ from threema.threemaapi import ThreemaAdminClient
 client = ThreemaAdminClient()
 changes = client.findNormalizations()
 
+now = datetime.datetime.now()
 updates = changes.get("updates", [])
-print(f"\n*** Found {len(updates)} updates\n")
-for update in updates:
-    threemaId = update.get("threemaId")
-    firstname = update.get("firstNameNormalized")
-    lastname = update.get("lastNameNormalized")
-    enabled = update.get("enabled", True)
-    print(
-        f"Updating threemaId {threemaId} to {firstname} {lastname}, enabled: {enabled}")
-    client.applyContactChange(threemaId, firstname, lastname, enabled)
+print(f"\nSync running at {now.strftime('%d.%m.%y %H:%M')}")
+
+if updates:
+    print(f"*** Found {len(updates)} updates\n")
+    for update in updates:
+        threemaId = update.get("threemaId")
+        firstname = update.get("firstNameNormalized")
+        lastname = update.get("lastNameNormalized")
+        enabled = update.get("enabled", True)
+        print(
+            f"Updating threemaId {threemaId} to {firstname} {lastname}, enabled: {enabled}")
+        client.applyContactChange(threemaId, firstname, lastname, enabled)
 
 missing = changes.get("missing", [])
-print(f"\n*** Found {len(missing)} missing contacts\n")
-for m in missing:
-    threemaId = m.get("threemaId")
-    firstName = m.get("firstName")
-    lastName = m.get("lastName")
-    print(
-        f"Creating contact for threemaId {threemaId} with firstname {firstName} and lastname {lastName}")
-    client.createContact(threemaId, firstName, lastName)
+if missing:
+    print(f"\n*** Found {len(missing)} missing contacts\n")
+    for m in missing:
+        threemaId = m.get("threemaId")
+        firstName = m.get("firstName")
+        lastName = m.get("lastName")
+        print(
+            f"Creating contact for threemaId {threemaId} with firstname {firstName} and lastname {lastName}")
+        client.createContact(threemaId, firstName, lastName)
+
+if not missing and not updates:
+    print("Everything in sync, nothing to do here.")
