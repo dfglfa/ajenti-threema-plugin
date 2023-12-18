6864b205861deb9a3f6b31787131c0d62e57a0c2
jcharra <johannes.charra@gmail.com>
2023-12-18 22:31:37 +0100

    dismiss container on finish

--- run_dockerized.sh
+++ run_dockerized.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it -p 8000:8000 -v $PWD:/opt/plugins/ajenti-threema -e THREEMA_CONFIG=/opt/plugins/ajenti-threema/threema.yml ajenti_plugin_dev
+docker run --rm -it -p 8000:8000 -v $PWD:/opt/plugins/ajenti-threema -e THREEMA_CONFIG=/opt/plugins/ajenti-threema/threema.yml ajenti_plugin_dev
