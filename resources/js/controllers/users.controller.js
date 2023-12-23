19d18b8910db2c496f876aa18656107ae9407312
jcharra <johannes.charra@gmail.com>
2023-12-23 14:18:14 +0100

    fix navigation, mark users area as WIP

--- resources/js/controllers/users.controller.js
+++ resources/js/controllers/users.controller.js
@@ -1,1 +1,3 @@
-angular.module("dfglfa.threema_connector").controller("ThreemaUsersController", function ($scope, $http, classService) {});
+angular.module("dfglfa.threema_connector").controller("ThreemaUsersController", function ($scope, $http, classService) {
+  $scope.users = [];
+});
